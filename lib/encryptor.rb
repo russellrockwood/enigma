5c2752e780e56b8e2aa51b7a3f5df0dd5f4a5b62
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-12 11:07:25 -0500

    add notes, change encryptor class to module

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,4 +1,11 @@
 
-class Encryptor
+module Encryptor
 
 end
+
+
+# You should be able to generate random numbers with leading zeros. In the example shown, the random number was 2715, so it should be zero padded to make it a five digit number (02715)
+
+# Your translation should only translate using lowercase letters. If a letter is uppercase, it should use the lowercase translation, so in the example above, “HELLO WORLD” would still translate to “keder ohulw”
+
+# If you encounter a character not in the character set, it should be translated as itself. Using the example above, “hello world!” should be translated as “keder ohulw!”
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,34 @@
-class Enigma
+require './lib/encryptor'
 
+class Enigma
+  include Encryptor
   # def initialize
   #
   # end
 
-  
+  def encrypt(message, key, date)
+     # If the key is not included, generate a random key.
+
+     # If the date is not included, use today’s date.
+
+     # The encrypt method returns a hash with three keys:
+
+    # :encryption => the encrypted String
+    # :key => the key used for encryption as a String
+    # :date => the date used for encryption as a String in the form DDMMYY
+  end
+
+  def decrypt(ciphertext, key, date)
+    # The decrypt method takes a ciphertext String and the Key used for encryption as arguments.
+
+    # The decrypt method can optionally take a date as the third argument.
+
+    # If no date is given, this method should use today’s date for decryption.
+
+    # The decrypt method returns a hash with three keys:
+
+    # :decryption => the decrypted String
+    # :key => the key used for decryption as a String
+    # :date => the date used for decryption as a String in the form DDMMYY
+  end
 end
