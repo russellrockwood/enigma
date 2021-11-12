63b2460f3c623b8d0f6b49694c121c56de275e1e
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-12 11:42:23 -0500

    add get_keys_test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,11 +3,23 @@ require './lib/enigma'
 describe Enigma do
   before(:each) do
     @enigma = Enigma.new
+    @test_key = 02715
+    @test_date = 040895
   end
 
   it 'exists' do
     expect(@enigma).to be_a(Enigma)
   end
 
-  
+  it 'creates key hash' do
+    expected = {
+      :a => 02,
+      :b => 27,
+      :c => 71,
+      :d => 15
+     }
+
+     expect(@enigma.get_keys(@test_key)).to eq(expected)
+  end
+
 end
