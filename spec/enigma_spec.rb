e86f4fdb3b91f09b3ae2462a3f8960e6644d8d7b
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-12 13:20:54 -0500

    update get_shifts test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,23 +3,18 @@ require './lib/enigma'
 describe Enigma do
   before(:each) do
     @enigma = Enigma.new
-    @test_key = 02715
-    @test_date = 040895
+    @test_key = '02715'
+    @test_date = '040895'
   end
 
   it 'exists' do
     expect(@enigma).to be_a(Enigma)
   end
 
-  it 'creates key hash' do
-    expected = {
-      :a => 02,
-      :b => 27,
-      :c => 71,
-      :d => 15
-     }
+  it 'creates array with proper shifts' do
+    expected = [3, 27, 73, 20]
 
-     expect(@enigma.get_keys(@test_key)).to eq(expected)
+     expect(@enigma.get_shifts(@test_key, @test_date)).to eq(expected)
   end
 
 end
