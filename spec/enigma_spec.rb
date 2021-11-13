5501f1e439f8cb1af0fbfc6f1eda855be9a57a36
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-13 09:54:52 -0500

    add shift_letter test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,18 +12,29 @@ describe Enigma do
   end
 
   it 'creates array with proper shifts' do
-    expected = [3, 27, 73, 20]
+    expected = [3, 0, 19, 20]
 
      expect(@enigma.get_shifts(@test_key, @test_date)).to eq(expected)
   end
 
-  it 'shifts letters correctly' do
-    string_1 = 'hell'
-    string_2 = 'o wo'
-    shifts = @enigma.get_shifts(@test_key, @test_date)
+  it 'shifts single letters correctly' do
+    letter = 'h'
+    letter_2 = 'l'
 
-    expect(@enigma.shift_four(string_1, shifts)).to eq('kede')
-    expect(@enigma.shift_four(string_2, shifts)).to eq('r oh')
+    shift = 3
+    shift_2 = 19
+
+    expect(@enigma.shift_letter(letter, shift)).to eq('k')
+    expect(@enigma.shift_letter(letter_2, shift_2)).to eq('d')
   end
+
+  # it 'shifts letters correctly' do
+  #   string_1 = 'hell'
+  #   string_2 = 'o wo'
+  #   shifts = @enigma.get_shifts(@test_key, @test_date)
+  #
+  #   expect(@enigma.shift_four(string_1, shifts)).to eq('kede')
+  #   expect(@enigma.shift_four(string_2, shifts)).to eq('r oh')
+  # end
 
 end
